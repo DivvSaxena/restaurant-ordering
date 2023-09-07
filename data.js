716928587af5cca1a5ce671a66c948b5be2a2d4e
Divv Saxena <saxenadivv@gmail.com>
2023-09-07 19:02:51 +0530

    Final product ready

--- data.js
+++ data.js
@@ -3,14 +3,14 @@ export const menuArray = [
         name: "Pizza",
         ingredients: ["pepperoni", "mushrom", "mozarella"],
         id: '0',
-        price: 14,
+        price: 12,
         emoji: "🍕",
         location: 'images/item1.jpg'
     },
     {
         name: "Hamburger",
         ingredients: ["beef", "cheese", "lettuce"],
-        price: 12,
+        price: 10,
         emoji: "🍔",
         id: '1',
         location: 'images/item2.jpg'
@@ -18,7 +18,7 @@ export const menuArray = [
         {
         name: "Beer",
         ingredients: ["grain, hops, yeast, water"],
-        price: 12,
+        price: 14,
         emoji: "🍺",
         id: '2',
         location: 'images/item3.png'
